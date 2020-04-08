46132a4e3360b224eaa4a3ccbeb225fdd22cceb8
pedrognicolau <61878026+pedrognicolau@users.noreply.github.com>
2020-04-08 11:36:21 +0200

    Add files via upload

---
+++ scripts/camera_data_processing.R
@@ -0,0 +1,123 @@
+wd <- "/Users/pedronicolau/OccupancyAbundanceCalibration/data/cameratrap/porsanger"
+setwd(wd)
+dir()
+library(dplyr)
+require(lubridate)
+ctdata <- read.csv(dir()[1])
+
+filename <- ctdata$fileName[100]
+
+datesplit <- function(filename)  # retrieve date and station from file name
+{
+  filename2 <- as.character(filename)
+  string <- strsplit(filename2,"_")
+  
+  station1 <- string[[1]][2]
+  station2 <- strsplit(station1,"/")[[1]]
+  station3 <- station2[length(station2)]
+  
+  #station4 <- strsplit(station3,"")[[1]]
+  #station5 <- paste0(station4[2],station4[3])
+  #station <- as.numeric(station5)
+  
+  
+  date1<- string[[1]][3]
+  date2 <- strsplit(date1,"-")
+  outputdata <- c(station3,date2[[1]],date1)
+  return(outputdata)
+}
+
+# join dataset with camera info 
+datas <- as.data.frame(t(sapply(ctdata$fileName,datesplit)))
+colnames(datas) <- c("station","year","month","day","date")
+
+factor2number <- function(x) return(as.numeric(as.character(x)))
+
+datas[,2:4] <- sapply(datas[,2:4], factor2number)
+datas[,c(1,5)] <- sapply(datas[,c(1,5)], as.character)
+
+ctdata2 <- data.frame(datas,ctdata[,4:13])
+ctdata2$answer <- ifelse(ctdata2$confidence1>0.75,ctdata2$guess1,0)
+
+ctdatax <- ctdata2[,c(1:5,11,16)]
+ctdatax$animal <- ifelse(ctdatax$answer>2,1,0)
+agganimal <- aggregate(animal~date, data=ctdatax,mean)
+agganimal$date <- as.Date(agganimal$date)
+agganimal$julian <- julian(as.Date(agganimal$date), origin=as.Date("2018-06-16"))
+agganimal$month <- months(as.Date(agganimal$date))
+
+plot(animal ~ julian, data=agganimal, xaxt = "n", pch=19, ylab="Proportion of photos with id'ed animal",xlab="months")
+axis(1, at=seq(1+15,480-15,30),
+     labels = agganimal$month[seq(1+15,480-15,30)], cex.axis = .7)
+
+
+ctdata3 <- ctdata2[ctdata2$answer>1,c(1:5,11,16)]
+labels <- c("empty","bird","vole","least_weasel","lemming","shrew","stoat")
+whichsp <- function(number) labels[number]
+ctdata3$species <- sapply(ctdata3$answer, FUN=whichsp)
+
+head(ctdatax)
+nrow(filter(ctdatax,answer==3))
+nrow(volescr)
+
+
+
+aggct1 <- aggregate(count~station+year+month+species, data=ctdata3, FUN=sum)
+
+aggct2 <- arrange(aggct1,species,station,year,month)
+
+
+#### vole data ####
+timeframe <- data.frame(daysofyear=1:366,weeks=c(rep.int(1:52,7),53,53))
+
+volescr <- filter(ctdata3,species=="vole")
+unique(sort(volescr$date))
+volescr$julianday <- julian(as.Date(volescr$date), origin=as.Date("2018-06-15"))
+volescr$dayofyear <- julian(as.Date(volescr$date), origin=as.Date("2018-06-"))
+
+volescr$month <- months(as.Date(volescr$date))
+volescr$week <- strftime(as.Date(volescr$date), format = "%V")
+
+
+
+stations <- unique(volescr$station)
+aggvoles <- aggregate(count~julianday+month, data=volescr,FUN=sum)
+
+monthlab <- c("July",      "August",    "September", "October",   "November",  "December",  "January",   "February", 
+              "March",     "April",     "May",       "June",      "July", "August", "September")
+axis(1, at=seq(1+15,480-15,30),
+     labels = monthlab, cex.axis = .7)
+
+aggvolesweek <- aggregate(count~week+year, data=volescr,FUN=sum)
+aggvolesweek$julianweek <- aggvolesweek$jweek
+
+
+for(st in 1:length(stations)){
+volesg00 <- filter(volescr, station==stations[st])
+print(stations[st])
+print(sum(volesg00$count))
+}
+
+volesg00$timepoint <- 1:nrow(volesg00)
+
+plot(count~timepoint,data=volesg00,pch=19, xaxt="n", xlab="month", type="b")
+axis(1, at=volesg01$timepoint, labels = volesg01$month, cex.axis = .7)
+
+unique(volesg00$station)
+volesg01$date <- as.Date(volesg01$date, "%m/%Y")
+
+plot(count ~ date, data=volesg01, xaxt = "n", pch=19)
+sort(unique(KM10$station))
+sort(stations)
+KM10 <- KM7[KM7$station%in%stations,]
+crcounts <- aggregate(count~trapseason+date,data=KM10,sum)
+crcounts$julianday <- julian(as.Date(crcounts$date), origin=as.Date("2018-06-15"))
+
+plot(crcounts$julianday,crcounts$count,pch=19,cex=2,col="red",xlim=c(0,452),ylim=c(0,310),
+     ylab="Counts",xaxt="n", xlab="")
+points(aggvoles$julianday, aggvoles$count, pch=19,cex=1)
+monthlab <- c("J",      "A",    "S", "O",   "N",  "D",  "J",   "F", 
+              "M",     "A",     "M",       "J",      "J", "A", "S")
+legend(226,300,legend=c("Photos","CR Counts"),pch=19, col=c("black","red"),bty="n")
+axis(1, at=seq(1+15,480-15,30),
+     labels = monthlab, cex.axis = .7)
---
+++ scripts/cr_data_processing.R
@@ -0,0 +1,72 @@
+### JOIN CAPTURE RECAPTURE DATASETS ###
+# import porsanger data
+# set working dir
+wd <- "/Users/pedronicolau/OccupancyAbundanceCalibration/data/capture_recapture/porsanger"
+setwd(wd)
+
+# set up command not in
+'%!in%' <- function(x,y)!('%in%'(x,y))
+library(dplyr)
+
+# go through the different years in folder porsanger
+# to retrieve the data and lump them together
+ds <- 0 # set counter
+porsdset <- list() #empty list
+for(yr in 1:length(dir())) #check year in directory
+{
+  #set working directory in year yr
+  getwd() 
+  year <- as.numeric(dir()[yr]) # retrive year as numeric from name
+  setwd(paste0(wd,"/",dir()[yr])) 
+  
+  # creating list with all the data frames
+  for(dset in 1:length(dir()))
+  {
+    ds <- ds+1 # increase counter
+    # read data
+    data1 <- read.table(dir()[dset], header=T, sep="\t", stringsAsFactors = FALSE)
+    # add year
+    data1$year <- year
+    # attach to list
+    porsdset[[ds]] <- data1
+  }
+  setwd(wd)
+  
+}
+
+# go through lists
+for(df in 1:length(porsdset))
+{
+  dataf <- porsdset[[df]]
+  
+  # just because not all dataframes are the same
+  if(ncol(dataf) > 16 ) dataf <- dataf[,1:16] #
+  if(ncol(dataf) < 16 ) dataf <- data.frame(dataf[,1:8],indnum_old=NA,dataf[,9:15]) #reordering columns
+  
+  # to start off the data frame
+  if ( df == 1 )
+  {
+    jointdata <- dataf
+    rnames <- names(jointdata)
+  }
+  
+  if ( df != 1 )
+  {
+      colnames(dataf) <- rnames
+      jointdata <- rbind(jointdata,dataf)
+  }
+  print(nrow(jointdata))
+
+  }
+# uniformize names
+jointdata$transect[jointdata$transect%!in%c("1","2","3","4","5")] <- "MASOY"
+
+## split datasets by transect ##
+masdata <- filter(jointdata, transect=="MASOY")
+pordata <- filter(jointdata, transect!="MASOY")
+porstation <- paste0("T",pordata$transect,"-",pordata$station)
+pordata$station <- porstation
+
+
+crdata <- rbind(masdata,pordata)
+write.csv2(crdata,"/Users/pedronicolau/OccupancyAbundanceCalibration/data/joint_CRDATA.csv")
