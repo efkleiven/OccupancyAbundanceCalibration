224bef074452e870560ec1518e9a15beaec412dc
efkleiven <eivind.f.kleiven@uit.no>
2020-11-10 16:13:35 +0100

    Cleaned 2_CRdata_processing.R. Some changes where done to assigning idividual numbers to unmarked individuals.

--- scripts/2_CRdata_processing.R
+++ scripts/2_CRdata_processing.R
@@ -4,9 +4,10 @@ library(dplyr)
 #read data
 #KMdata <- read.csv2("/Users/pedronicolau/OccupancyAbundanceCalibration/data/joint_CRDATA.csv", stringsAsFactors = FALSE)
 KMdata <- read.csv2("C:/Eivind/GitProjects/OccupancyAbundanceCalibration/data/joint_CRDATA.csv", stringsAsFactors = FALSE)
-
+?read.csv2
 #get rid of NAs
 KMdata1 <- KMdata
+
 #KMdata1$weight <- as.numeric(KMdata1$weight) #this introduces a lot of NA's
 
 sum(is.na(KMdata1$weight))
@@ -15,6 +16,7 @@ sum(is.na(KMdata1$weight))
 KMdata1$weight[KMdata1$weight=="<12"] <- 11
 KMdata1$weight[KMdata1$weight=="<10"] <- 10
 KMdata1$weight[KMdata1$weight==">10"] <- 10
+
 unique(KMdata1$weight)
 
 #make NA's a character to be able to run nchar
@@ -27,6 +29,7 @@ if(nchar(KMdata1$weight[i])>2) {
 
 # set NA's back to NA
 KMdata1$weight[KMdata1$weight=="mi"] <- NA
+KMdata1$weight <- as.numeric(KMdata1$weight)
 
 unique(KMdata1$datetime)
 
@@ -58,18 +61,19 @@ tseason <- distinct(KMdata1,year,date)
 tseason2 <- arrange(tseason,date)
 tseason2$trapseason <- sort(rep.int(1:(nrow(tseason2)/2),2))
 
+# add trap season
 KM1 <- left_join(KMdata1,tseason2)
-unique(KM1$species)
+
+# fix names
 KM1$species[KM1$species=="R\303\230YSKATT"] <- "ROYSKATT"
 KM1$species[KM1$species=="SN\330MUS"] <- "SNOMUS"
 
-head(KM1)
+# how many individuals are not labeled
 no_NAs <- length(KM1$Nindnum[is.na(KM1$Nindnum)==TRUE])
-head(KM1)
+
 ### create labels for the NAs ##
 NA_label <- c()
 for(i in 1:no_NAs) NA_label[i] <- paste0("UNMNA",i)
-
 KM1$Nindnum[is.na(KM1$Nindnum)==TRUE] <- NA_label
 
 # function to know if it's untagged
@@ -83,36 +87,43 @@ isitU <- function(code)
 }
 
 ## NON TAGGED ##
+unique(KM1$Nindnum[KM1$tagged==0])
+
 KM1$tagged <- 1
 # if it is untagged, mark as 0
 KM1$tagged[sapply(KM1$Nindnum,isitU)==TRUE] <- 0
 
+# MAKE SURE EACH UNMARKED HAS ITS OWN LABEL
+# AN UNMARKED IS CONSIDERED TO HAVE THE FIRST LETTER AS U
+no_Us <- nrow(KM1[KM1$tagged==0,])
+NA_Ulabel <- c()
+for(i in 1:no_Us) NA_Ulabel[i] <- paste0("UNM",i)
+KM1$Nindnum[KM1$tagged==0] <- NA_Ulabel
+
 # get month
 KM1$month <- months(KM1$date)
 
-
+table(KM1$weight[KM1$tagged==0])
 # %% MANUAL CHECK %% #
 #### UNTAGGED INDIVIDUALS WHICH WERE GIVEN SAME CODE ON SAME STATION ####
-# UNME
-nE <- nrow(KM1[KM1$Nindnum=="UNME",])
-Elab <- c()
-for(i in 1:nE) Elab[i] <- paste0("UNME",i)
-KM1$Nindnum[KM1$Nindnum=="UNME"] <- Elab
-# UNMJ
-nJ <- nrow(KM1[KM1$Nindnum=="UNMJ",])
-Jlab <- c()
-for(i in 1:nJ) Jlab[i] <- paste0("UNMJ",i)
-KM1$Nindnum[KM1$Nindnum=="UNMJ"] <- Jlab
+# # UNME
+# nE <- nrow(KM1[KM1$Nindnum=="UNME",])
+# Elab <- c()
+# for(i in 1:nE) Elab[i] <- paste0("UNME",i)
+# KM1$Nindnum[KM1$Nindnum=="UNME"] <- Elab
+# 
+# # UNMJ
+# nJ <- nrow(KM1[KM1$Nindnum=="UNMJ",])
+# Jlab <- c()
+# for(i in 1:nJ) Jlab[i] <- paste0("UNMJ",i)
+# KM1$Nindnum[KM1$Nindnum=="UNMJ"] <- Jlab
 
-# to remove *checked manually*
-rem_pos <- which(KM1$Nindnum=="0007D60D46"&KM1$seas=="SPRING")
-KM1.2 <- KM1[-rem_pos,]
-
+KM1.2 <- KM1
 
 #### repeated observation ####
 ### UNIQUE ID OF INDIVIDUALS ###
 uniqueid <- distinct(KM1.2,year,station,Nindnum,trapseason,transect,species)
-head(KM1)
+
 uniqueid$id <- 1:nrow(uniqueid)
 # attach labels for the different individuals according to
 KM2 <- left_join(KM1.2,uniqueid)
@@ -123,9 +134,14 @@ KM2 <- left_join(KM1.2,uniqueid)
 # 1+0 = 1 "10"
 # 0+2 = 2 "01"
 # 1+2 = 3 "11"
+
+# THERE ARE 5 FAKE INDIVIDUALS ? 
 idch <- aggregate(check~year+trapseason+station+tagged+id+species,data=KM2, FUN=sum)
+# there are 5 missing individuals
 nrow(idch)
-hist(idch$check)
+nrow(uniqueid)
+
+# KMdata1[KMdata$Nindnum=="0007D60D46",]
 colnames(idch)[ncol(idch)] <- "category"
 
 # attach capture history
@@ -156,8 +172,13 @@ KM7 <- left_join(KM6,tseason2[seq(1,nrow(tseason2)-1,2),])
 KM7$julian <- julian(as.Date(KM7$date), origin=as.Date("2018-06-15"))
 KM7$weekofyear <- as.numeric(strftime(as.Date(KM7$date), format = "%V"))
 table(KM7$species)
-head(KM7)
+
 write.csv2(KM7, "C:/Eivind/GitProjects/OccupancyAbundanceCalibration/data/CR_processed.csv")
 
-KM7$count <- 1
-crcounts <- aggregate(count~trapseason+date+station,data=KM7,sum)
+
+KM8 <- filter(KM7, !(species%in% c("ROYSKATT","SNOMUS")))
+KM8$count <- 1
+crcounts <- aggregate(count~trapseason+date+station,data=KM8,sum)
+summary(KM7)
+
+unique(KM7$date)
