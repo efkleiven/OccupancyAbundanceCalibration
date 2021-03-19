bb14dee6c98ab8528e68ff48f6c3c43e60927e48
pedrognicolau <pedrognicolau@gmail.com>
2021-03-19 16:26:14 +0100

    Additional corrections of haakoya data processing

--- scripts/Haakoya/cr_data_processing_haakoya.R
+++ scripts/Haakoya/cr_data_processing_haakoya.R
@@ -12,7 +12,8 @@ setwd(wd)
 library(dplyr)
 library("readxl")
 
-
+file_list <- c("fangst_håkøy_Rolf_18_modified.xls", "fangst_håkøy_Rolf_19_modified.xls", "fangst_håkøy_Rolf_20_modified.xls",
+ "jon_hakoya_modified.xls")  
 
 ############ Rolf data ############ 
 # 1. bind 3 separate files
@@ -25,7 +26,7 @@ hakoyRolf <- list() #empty list
 
 for(i in 1:3){
   # read data
-  hakoyRolf[[i]] <- read_xls(dir()[i])
+  hakoyRolf[[i]] <- read_xls(file_list[i])
 }
 # lump datasets 
 hakoyR <- bind_rows(hakoyRolf)
@@ -38,11 +39,13 @@ hakoyR2 <- select(hakoyR, date,period, trap,ind, sex, wgt,cap,com) # select vars
 trapsessR <- tibble(date=unique(hakoyR$date), trapsession = rep(1:15,each=3))
 hakoyR3 <- left_join(hakoyR2,trapsessR )
 
+# correct typo : 49 to 39
+fortyniner <- which(hakoyR3$trap==49)
+hakoyR3[fortyniner,"trap"] <- 39
 # 4. split traps into stations
-Rstations <- tibble(trap=1:45,station=c(rep(1,14),rep(2,7),rep(3,20),rep(2,4)))
+Rstations <- tibble(trap=1:45,station=c(rep(1,14),rep(2,7),rep(3,20),rep(2,4)), check=rep(1:3,15))
 hakoyR4 <- left_join(hakoyR3,Rstations)
-hakoyR4$trap <- as.character(hakoyR$trap )
-
+hakoyR4$trap <- as.character(hakoyR4$trap)
 ############  Jon Data  ############ 
 # 1. access file
 # 2. combine trapline and trap column into trap
@@ -53,7 +56,7 @@ hakoyR4$trap <- as.character(hakoyR$trap )
 # 7. add station variable
 
 # 1. access file
-hakoyJon <- read_xls(dir()[4]) # last file in directory
+hakoyJon <- read_xls(file_list[4]) # last file in directory
 # 2. combine trapline and trap column into trap
 hakoyJon$trap <- paste0(hakoyJon$trapline,hakoyJon$trapcolumn) #create station var
 # 3. select variables
@@ -63,7 +66,7 @@ hakoyJon2 <- select(hakoyJon, date,period, trap,ind, sex, wgt,cap,com, trapline)
 
 hakoyJon3 <- filter(hakoyJon2, date > "2018-01-01")
 # 5. obtain trapping session (different days)
-trapsessJ <- tibble(date=unique(hakoyJon3$date), trapsession = rep(1:15,each=3))
+trapsessJ <- tibble(date=unique(hakoyJon3$date), trapsession = rep(1:15,each=3), check=rep(1:3,15))
 hakoyJon4 <- left_join(hakoyJon3,trapsessJ )
 
 # 6. correct line
@@ -77,5 +80,12 @@ hakoyJon4$station <- 4
 #### bind Jon and Rolf ####
 
 hakoydat <- bind_rows(hakoyR4,hakoyJon4)
-tail(hakoydat,20)
-write.csv2(hakoydat,"haakoya_crdata_4stations.csv")
+
+#### correct minor fixes ####
+novoles <- which(hakoydat$ind=="no voles")
+hakoydat2 <- hakoydat[-novoles,]
+
+novoles2 <- which(hakoydat$ind=="NA")
+hakoydat3 <- hakoydat2[-novoles2,]
+
+write.csv2(hakoydat3,"haakoya_crdata_4stations.csv")
