86ae74082ba6292dee558df36672048883fb7f79
pedrognicolau <pedrognicolau@gmail.com>
2021-04-21 14:33:23 +0200

    estimate N for different Ns

--- scripts/Haakoya/detectability_estimation_haakoya.R
+++ scripts/Haakoya/detectability_estimation_haakoya.R
@@ -12,7 +12,7 @@ source("~/Documents/OccupancyAbundanceCalibration/scripts/2_2_1_functions.R")
 #### read datasets ####
 # count only data
 # capture history dataset formatted for INLA
-data4inla0 <- readRDS("~/Documents/OccupancyAbundanceCalibration/data/capture_recapture/haakoya/processed/haakoya_inlaformat.rds")
+data4inla0 <- readRDS("~/Documents/OccupancyAbundanceCalibration/data/capture_recapture/haakoya/processed/haakoya_inlaformat2.rds")
 # dataset with only observed capture histories
 summary(data4inla0)
 data4inla0$wgt[is.na(data4inla0$wgt)] <- mean(data4inla0$wgt, na.rm=TRUE)
@@ -106,86 +106,80 @@ abline(v=mean(1-totalvgam$p00.est))
 # boxplot(prob_df[,c(4,5)], ylim=c(.2,.75))
 
 # lump probabilities with individuals
-totaldf <- data.frame(rawdata[,1:8],p1.est=prob_df$p1_inla,
+totaldf <- data.frame(rawdata[,c(1:8,10:12)],p1.est=prob_df$p1_inla,
                       p2.est=prob_df$p2_inla, p3.est=prob_df$p3_inla,
                       p00.est=(prob_df$p00_inla))
 inladf <- arrange(totaldf, trapsession,station, id)
 
 # compute N using the HT estimator for each station and time point
-inlaframe <- partialN(p00.st.tp=data.frame(inladf$p00.est,inladf$station,inladf$trapsession))
-# add zero counts
-estimatedf <- as.data.frame.table(xtabs(N.est~timepoint+station, data=inlaframe))
-colnames(estimatedf)[3] <- "inlacr"
-estimatedf1 <- arrange(estimatedf,station,timepoint)
+# zeros already added
+# different Ns for different radius
+inlaframe <- partialN_radius(inladf, radius=TRUE)
 
 
 
 #### CR VGAM MODEL ####
 
-# try out different models
-library(VGAM)
-mod0 <- vglm(cbind(c1,c2,c3) ~ 1, posbernoulli.t, data = rawdata)
-mod1 <- vglm(cbind(c1,c2,c3) ~ wgt, posbernoulli.t, data = rawdata)
-mod2 <- vglm(cbind(c1,c2,c3) ~ wgt+sex, posbernoulli.tb, data = rawdata)
-summary(mod2)
+# # try out different models
+# library(VGAM)
+# mod0 <- vglm(cbind(c1,c2,c3) ~ 1, posbernoulli.t, data = rawdata)
+# mod1 <- vglm(cbind(c1,c2,c3) ~ wgt, posbernoulli.t, data = rawdata)
+# mod2 <- vglm(cbind(c1,c2,c3) ~ wgt+sex, posbernoulli.tb, data = rawdata)
+# summary(mod2)
+# 
+# # get fitted values from the VGAM model
+# totalvgam <- as.data.frame(cbind(rawdata,fitted(mod1)))
+# 
+# colnames(totalvgam)[(ncol(totalvgam)-2):(ncol(totalvgam))] <- c("p1","p2","p3")
+# totalvgam$p00.est <- (1-totalvgam$p1)*(1-totalvgam$p2)*(1-totalvgam$p3)
+# totalvgam$station <- factor(totalvgam$station, levels=unique(totalvgam$station))
+# vgamdf <- arrange(totalvgam, trapsession,station, id)
+# 
+# vgamframe <- partialN(p00.st.tp=data.frame(vgamdf$p00.est,vgamdf$station,vgamdf$trapsession))
+# 
+# 
+# # add zeros
+# vgamts <- as.data.frame.table(xtabs(N.est~timepoint+station, data=vgamframe))
+# vgamts <- arrange(vgamts,station,timepoint)
+# 
+# estimatedf2 <- left_join(estimatedf1,vgamts)
+# tibble(estimatedf2)
+# colnames(estimatedf2)[4] <- "vgamcr"
+# colnames(estimatedf2)[1] <- "trapsession"
+# estimatedf2$trapsession <- as.numeric(as.character(estimatedf2$trapsession))
+# 
+# 
+# plot(estimatedf2$inlacr, type="l")
+# lines(estimatedf2$vgamcr, type="l", col="blue")
 
-# get fitted values from the VGAM model
-totalvgam <- as.data.frame(cbind(rawdata,fitted(mod1)))
-
-colnames(totalvgam)[(ncol(totalvgam)-2):(ncol(totalvgam))] <- c("p1","p2","p3")
-totalvgam$p00.est <- (1-totalvgam$p1)*(1-totalvgam$p2)*(1-totalvgam$p3)
-tibble(totalvgam[,-2])
-hist(totalvgam$wgt)
-
-totalvgam$station <- factor(totalvgam$station, levels=unique(totalvgam$station))
-vgamdf <- arrange(totalvgam, trapsession,station, id)
-
-vgamframe <- partialN(p00.st.tp=data.frame(vgamdf$p00.est,vgamdf$station,vgamdf$trapsession))
-
-
-# add zeros
-vgamts <- as.data.frame.table(xtabs(N.est~timepoint+station, data=vgamframe))
-vgamts <- arrange(vgamts,station,timepoint)
-
-estimatedf2 <- left_join(estimatedf1,vgamts)
-tibble(estimatedf2)
-colnames(estimatedf2)[4] <- "vgamcr"
-colnames(estimatedf2)[1] <- "trapsession"
-estimatedf2$trapsession <- as.numeric(as.character(estimatedf2$trapsession))
-
-
-plot(estimatedf2$inlacr, type="l")
-lines(estimatedf2$vgamcr, type="l", col="blue")
-
+##### RAW COUNTS ######
 # add raw counts
 # add zeros if any
 rawts <- as.data.frame(xtabs(count~trapsession+station, data=rawdata))
 rawts2 <- arrange(rawts,station,trapsession)
 colnames(rawts2)[3] <- "counts"
-rawts2$trapsession <- as.numeric(rawts2$trapsession)
+rawts2 <- as.data.frame(apply(rawts2[,], 2, FUN=as.numeric))
 
 
-estimatedf3 <- left_join(estimatedf2,rawts2)
-
+estimatedf3 <- left_join(inlaframe,rawts2)
 estimatedf4 <- tibble(estimatedf3)
-estimatedf5 <- data.frame(apply(estimatedf3,2,as.numeric))
-saveRDS(estimatedf5, "~/Documents/OccupancyAbundanceCalibration/data/capture_recapture/haakoya/processed/haakoya_abundance.rds")
+saveRDS(estimatedf4, "~/Documents/OccupancyAbundanceCalibration/data/capture_recapture/haakoya/processed/haakoya_abundance_radius.rds")
 
 
 ### PLOT ###
-par(mfrow=c(2,2))
-for(i in 1:4)
-{
-
-  plot(1:15,estimatedf5$inlacr[estimatedf5$station==i], 
-       col="red", type="l", main=i, ylim=c(0,50),
-       xlab="timepoint", ylab="count")
-  lines(1:15,estimatedf5$vgamcr[estimatedf5$station==i], 
-       col="blue", type="l", main=i, ylim=c(0,50),
-       xlab="timepoint", ylab="count")
-  lines(1:15,estimatedf5$counts[estimatedf5$station==i], 
-        col="black", type="l", main=i, ylim=c(0,50),
-        xlab="timepoint", ylab="count")
-  if(i==2) legend("topright",c("CRINLA","CRVGAM","RAW"), col=c("red","blue",1), lty=1)
-}
+# par(mfrow=c(2,2))
+# for(i in 1:4)
+# {
+# 
+#   plot(1:15,estimatedf5$inlacr[estimatedf5$station==i], 
+#        col="red", type="l", main=i, ylim=c(0,50),
+#        xlab="timepoint", ylab="count")
+#   lines(1:15,estimatedf5$vgamcr[estimatedf5$station==i], 
+#        col="blue", type="l", main=i, ylim=c(0,50),
+#        xlab="timepoint", ylab="count")
+#   lines(1:15,estimatedf5$counts[estimatedf5$station==i], 
+#         col="black", type="l", main=i, ylim=c(0,50),
+#         xlab="timepoint", ylab="count")
+#   if(i==2) legend("topright",c("CRINLA","CRVGAM","RAW"), col=c("red","blue",1), lty=1)
+# }
 
