1e236afe21bc64ab2317a7321342510cea0ba69d
pedrognicolau <pedrognicolau@gmail.com>
2021-07-21 20:55:19 +0200

    calibration analysis

--- scripts/Calibration/Calibration_regression.R
+++ scripts/Calibration/Calibration_regression.R
@@ -4,10 +4,14 @@ GRDF <- readRDS("data/cameratrap/porsanger/processed/GR_regression_data_porsange
 
 
 ### REGRESSION ####
-filtdataab <- filter(ABDF, species=="GRAASIDEMUS")
-filtdatagr <- filter(GRDF, species=="GRAASIDEMUS")
+library(dplyr)
 
-i=1
+# for trap season 1, we have missing values for all the photos taken before the trapping
+# for the GR dataset, trapseason 2 correponds to season 2 - season 1, thus it needs to be removed
+filtdataab <- filter(ABDF, species=="GRAASIDEMUS" & trapseason > 1)
+filtdatagr <- filter(GRDF, species=="GRAASIDEMUS" & trapseason > 2)
+
+
 modcoefs <- matrix(NA,ncol=3,nrow=13)
 for(i in 1:13)
 {
@@ -28,8 +32,8 @@ for(i in 1:13)
 }
 
 
-filtdataab <- filter(jointset4, species=="ROEDMUS" & trapseason > 1)
-filtdatagr <- filter(grdf2, species=="ROEDMUS" & trapseason > 2)
+filtdataab <- filter(ABDF, species=="ROEDMUS" & trapseason > 1)
+filtdatagr <- filter(GRDF, species=="ROEDMUS" & trapseason > 2)
 
 i=1
 modcoefs_RM <- matrix(NA,ncol=3,nrow=13)
