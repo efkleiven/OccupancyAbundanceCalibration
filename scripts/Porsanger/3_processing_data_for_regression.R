8b0f9443aecbcd9e42d3cd02dc2bd585423b3ea8
pedrognicolau <pedrognicolau@gmail.com>
2021-07-21 20:54:52 +0200

    data processing fixes

--- scripts/Porsanger/3_processing_data_for_regression.R
+++ scripts/Porsanger/3_processing_data_for_regression.R
@@ -10,10 +10,10 @@ library(scales)
 #cameradata <- read.csv2("/Users/pedronicolau/OccupancyAbundanceCalibration/data/camera_data_075confidence_processed.csv", stringsAsFactors = FALSE)
 #abundance <- readRDS("/Users/pedronicolau/OccupancyAbundanceCalibration/data/estimated_abundance.rds")
 getwd()
+
 livetrapdata <- read.csv2("./data/CR_processed.csv", stringsAsFactors = FALSE)
 # cameradata <- read.csv2("./data/camera_data_075confidence_processed.csv", stringsAsFactors = FALSE)
 cameradata <- readRDS("data/cameratrap/porsanger/processed/porsanger_camera_processing1.rds")
-getwd()
 abundance <- readRDS("data/capture_recapture/porsanger/porsanger_abundance_perspecies.rds")
 
 abundance[,3:4] <- round(abundance[,3:4],0)
@@ -28,12 +28,13 @@ cameradata$station[cameradata$station=="T5.2"] <- "T5-2"
 
 
 #### TIME FORMATTING ####
+# source formatGstations function
 cameradata$station <- sapply(cameradata$station, formatGstations) # this function is in 1_reading_CR_data.R
 unique(cameradata$station)
 ctstations <- unique(cameradata$station)
 
+# filter out stations without camera data
 livedata <- filter(abundance, station %in% ctstations)
-# source formatGstations function
 
 # which days do we use for comparison
 livedata$julian <- julian(livedata$datetime,origin = as.Date("2018-06-01"))
@@ -49,22 +50,8 @@ for (i in 1:d)
 }
 jdays <- sort(jdays0)
 
-#jdays <- c(unique(livetrapdata$julian),   # day 1 of cr experiment
-#           unique(livetrapdata$julian)+1, # day 2 of cr experiment
-#           unique(livetrapdata$julian)-1, # day 0 before cr experiment
-#           unique(livetrapdata$julian)+2, # day 1 after cr experiment
-#           unique(livetrapdata$julian)-2, # day 2 before cr experiment
-#           unique(livetrapdata$julian)+3  # day 2 after cr experiment
-#           )
-
-#
 jlength <- length(unique(livedata$julian))
 
-# getting labels for the given days
-
-#jlabels <- c(rep("01",jlength),rep("02",jlength),rep("b1",jlength),rep("a1",jlength),
-#             rep("b2",jlength),rep("a2",jlength)
-#             )
 jlabels <- paste0("d",rep(-d:d, jlength))
 
 # format days in which trapping was conducted
@@ -135,7 +122,6 @@ full_df$species[full_df$species=="G"] <- "GRAASIDEMUS"
 full_df$species[full_df$species=="M"] <- "MARKMUS"
 full_df$species[full_df$species=="R"] <- "ROEDMUS"
 
-unique(livextabs$species)
 unique(full_df$species)
 
 # once again, add all combinations in such a way to get the zero captures
@@ -146,39 +132,46 @@ colnames(livextabs)[4] <- "Abundance_HT"
 
 livextabs3 <- tibble::add_column(livextabs, counts = livextabs2$Freq, .after = 4)
 
-
 full_df2 <- filter(full_df, species %in% c("GRAASIDEMUS","MARKMUS","ROEDMUS"))
 fd1 <- arrange(full_df2, species,station,trapseason)
-livextabs4 <- filter(livextabs3, species != "FJELLROTTE")
 jointset <- left_join(full_df2,livextabs3)
 jointset2 <- arrange(jointset, species,station,trapseason)
 jointset3 <- jointset2[,c(1:3,(ncol(jointset2)-1),ncol(jointset2),4:(ncol(jointset2)-2))]
-# add mean count for two days after 
-
 jointset4 <- jointset3[,1:5]
 # day zero is day of first capture
 # day -1 is day of setting traps
 # day -2 is day before traps
-jointset4$previousday <- (data.frame(jointset3)[,16]) # day-2
+jointset4$settingday <- (data.frame(jointset3)[,17]) # day-1
+jointset4$experimdays.sum <- apply(jointset3[,17:19],1,sum) # day-1 to day 1
 jointset4$previous3day.sum <- apply(jointset3[,14:16],1,sum) # day -2 to -4
 jointset4$previous5day.sum <- apply(jointset3[,12:16],1,sum)
 jointset4$previous10day.sum <- apply(jointset3[,7:16],1,sum)
-jointset4$'5daysperiod.sum' <- apply(jointset3[,15:19],1,sum)
-jointset4$'9daysperiod.sum' <- apply(jointset3[,14:22],1,sum)
-jointset4$'18daysperiod.sum' <- apply(jointset3[,9:26],1,sum)
-jointset4$'5daysperiod.med' <- apply(jointset3[,15:19],1,median)
-jointset4$'9daysperiod.med' <- apply(jointset3[,14:22],1,median)
-jointset4$'18daysperiod.med' <- apply(jointset3[,19:26],1,median)
-jointset4$'5daysperiod.mean' <- apply(jointset3[,15:19],1,mean)
-jointset4$'9daysperiod.mean' <- apply(jointset3[,14:22],1,mean)
-jointset4$'18daysperiod.mean' <- apply(jointset3[,19:26],1,mean)
-
+jointset4$'5daysperiod.sum' <- apply(jointset3[,16:20],1,sum)
+jointset4$'9daysperiod.sum' <- apply(jointset3[,15:23],1,sum)
+jointset4$'19daysperiod.sum' <- apply(jointset3[,9:27],1,sum)
+jointset4$'5daysperiod.med' <- apply(jointset3[,16:20],1,median)
+jointset4$'9daysperiod.med' <- apply(jointset3[,15:23],1,median)
+jointset4$'19daysperiod.med' <- apply(jointset3[,9:27],1,median)
+jointset4$'5daysperiod.mean' <- apply(jointset3[,16:20],1,mean)
+jointset4$'9daysperiod.mean' <- apply(jointset3[,15:23],1,mean)
+jointset4$'19daysperiod.mean' <- apply(jointset3[,9:27],1,mean)
+#### centering on day of setting traps ####
+# jointset4$'5daysperiod.sum' <- apply(jointset3[,15:19],1,sum)
+# jointset4$'9daysperiod.sum' <- apply(jointset3[,14:22],1,sum)
+# jointset4$'18daysperiod.sum' <- apply(jointset3[,9:26],1,sum)
+# jointset4$'5daysperiod.med' <- apply(jointset3[,15:19],1,median)
+# jointset4$'9daysperiod.med' <- apply(jointset3[,14:22],1,median)
+# jointset4$'18daysperiod.med' <- apply(jointset3[,19:26],1,median)
+# jointset4$'5daysperiod.mean' <- apply(jointset3[,15:19],1,mean)
+# jointset4$'9daysperiod.mean' <- apply(jointset3[,14:22],1,mean)
+# jointset4$'18daysperiod.mean' <- apply(jointset3[,19:26],1,mean)
+#####
 saveRDS(jointset4, "data/cameratrap/porsanger/processed/regression_data_porsanger.rds")
 
 ### GROWTH RATES
 
 jointset5 <- arrange(jointset4, species,station,trapseason )
-# initiate data frame with 56 rows (14 timepoints per 4 station - 1)
+# initiate data frame with 360 rows (14 timepoints per 9 stations per 3 species)
 grdf <- matrix(nrow=360, ncol=ncol(jointset5))
 # get labeling from 14 rows per station
 row2remove <- (1:405)[-seq(1,nrow(jointset5),9)]
@@ -194,10 +187,10 @@ for(sp in 1:3)    {
   d0 <- filter(jointset5, species == speclab[sp])
 
   for (st in 1:15) {
-      d1 <- filter(d0, station == stalab[st])
-      grdf[nr:(nr + 7), 4:ncol(grdf)] <-
-        diff(as.matrix(log(d1[, 4:ncol(d1)] + 1)))
-      nr = nr + 8
+    d1 <- filter(d0, station == stalab[st])
+    grdf[nr:(nr + 7), 4:ncol(grdf)] <-
+      diff(as.matrix(log(d1[, 4:ncol(d1)] + 1)))
+    nr = nr + 8
     }
 }
 
@@ -205,7 +198,7 @@ grdf2 <- as.data.frame(grdf)
 
 # convert to numeric
 grdf2[,4:ncol(grdf2)] <- sapply(grdf2[,4:ncol(grdf2)], as.numeric)
-
+tail(grdf2,50)
 
 saveRDS(grdf2, "data/cameratrap/porsanger/processed/GR_regression_data_porsanger.rds")
 
--- scripts/Porsanger/Camera/2_camera_data_preprocessing.R
+++ scripts/Porsanger/Camera/2_camera_data_preprocessing.R
@@ -1,67 +1,58 @@
-setwd("..")
+setwd("~/OccupancyAbundanceCalibration/data")
 getwd()
 library(dplyr)
-porscamera <- readRDS("data/cameratrap/porsanger/processed/porsanger_imported.rds")
-tibble(porscamera)
+porscamera <- readRDS("cameratrap/porsanger/processed/porsanger_imported.rds")
+
+# we just assume that the first observation is always best
+pors0 <- filter(arrange(porscamera,Location, DateTimeOriginal), Trigger == "M 1/2")
+
 
 # obtain aggregated time
-porscamera$DateTimeOriginal <- paste0(porscamera$Date," ",porscamera$Time)
-# format as date
-porscamera$DateTimeOriginal <- strptime(porscamera$DateTimeOriginal,
-                                        format="%d.%m.%Y %H:%M:%S", tz="CET") #Central European Time
 
 # sort data by location and time
 # remove the photos triggered automatically
-pors1 <- filter(arrange(porscamera,Location, DateTimeOriginal), Trigger != "T")
-unique(porscamera$Trigger)
+# pors1 <- filter(arrange(porscamera,Location, DateTimeOriginal), Trigger != "T")
+
+
+
+
+
+
 # compute differences between adjacent times
-# differences at the borders of location should always be larger because the data is sorted by station
-difference <- diff(pors1$DateTimeOriginal)
-# which are due to camera settings
-dif1 <- which(abs(difference)<5)
-
-# slice set of "duplicated" rows
-duprows <- sort(c(dif1,dif1+1))
-duplicates <- pors1[duprows,]
-duplicates$id <- rep(1:(nrow(duplicates)/2), each=2)
-
-# # check short differences
-# dif2 <- which(abs(difference)%in%seq(2,59))
-# duprows2 <- sort(c(dif2,dif2-1,dif2+1))
-# ttt2 <- slice(pors1,duprows2)
-
-#diff(ttt2$DateTimeOriginal)
-
-## CHECK
-## check if there are individuals with different ids
-# duptest <- aggregate(species~id,data=duplicates, FUN=paste0, collapse="")
-# table(duptest$species)
-## there are not ! 
-
-# highest confidence is on even rows
-oddpos <- seq(1,nrow(duplicates),2)
-duplicates2 <- slice(duplicates, oddpos)
-
-## CHECK
-# length(unique(duplicates2$id))==nrow(duplicates)/2
-# nrow(duplicates2)/nrow(duplicates) == 0.5 # needs to be true: it is
-
-# remove now useless variable
-duplicates3 <- select(duplicates2, -"id")
-# retrieve non duplicates
-non_dups <- slice(pors1, -duprows)
-# join all datasets
-cameratrap1 <- arrange(bind_rows(duplicates3, non_dups), Location, DateTimeOriginal)
-
-# "_" separates station, apply function vectorially
+# differences at the borders of location should always be larger because the data is sorted by station and time
+# difference <- diff(pors1$DateTimeOriginal)
+# 
+# # which are due to camera settings
+# dif1 <- which(abs(difference)<3)
+# 
+# # slice set of "duplicated" rows
+# duprows <- unique(sort(c(dif1,dif1+1)))
+# duplicates <- pors1[duprows,]
+# 
+# duplicates2 <- filter(duplicates, Trigger == "M 1/2")
+# ## CHECK
+# # length(unique(duplicates2$id))==nrow(duplicates)/2
+# # nrow(duplicates2)/nrow(duplicates) == 0.5 # needs to be true: it is
+# 
+# # remove now useless variable
+# # retrieve non duplicates
+# non_dups <- pors1[-duprows,]
+# nrow(non_dups)+nrow(duplicates2)
+# nrow(pors1)-nrow(duplicates2)
+# # join all datasets
+# cameratrap1 <- arrange(bind_rows(duplicates2, non_dups), Location, DateTimeOriginal)
+cameratrap1 <- pors0
+# # "_" separates station, apply function vectorially
 get_station <- function(x) strsplit(x,"_")[[1]][1]
 cameratrap1$station <- sapply(cameratrap1$Location, get_station)
-cameratrap2 <- select(cameratrap1, -c(Date,Time,Location,Trigger))
+cameratrap2 <- select(cameratrap1, -c(Location,Trigger))
+# 
+# # should be true: it is
+# nrow(pors1)-nrow(duplicates)/2 == nrow(cameratrap2)
+# tibble(cameratrap2)
+# View(cameratrap2)
 
-# should be true: it is
-nrow(pors1)-nrow(duplicates)/2 == nrow(cameratrap1)
-tibble(cameratrap2)
 
-
+#saveRDS(cameratrap2, "cameratrap/porsanger/processed/porsanger_camera_processing1.rds")
 saveRDS(cameratrap2, "data/cameratrap/porsanger/processed/porsanger_camera_processing1.rds")
 
