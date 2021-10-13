e32a5663fe8eb5ede145f041dddf7244fb6d1788
pedrognicolau <pedrognicolau@gmail.com>
2021-10-13 18:56:33 +0200

    minor fixes

--- scripts/Haakoya/haakoya_regression_calibration.R
+++ scripts/Haakoya/haakoya_regression_calibration.R
@@ -1,5 +1,5 @@
-ABDF_h <- readRDS("data/calibration/Haakoya//regression_data_haakoya_ab_jul21.rds")
-
+ABDF_h <- readRDS("data/calibration/Haakoya/regression_data_haakoya_ab_jul21.rds")
+ABDF_hm <- readRDS("data/calibration/Haakoya/regression_data_haakoya_ab_mean.rds")
 GRDF_h <- readRDS("data/calibration/Haakoya/GR_regression_data_haakoya_jul21.rds")
 getwd()
 
@@ -10,7 +10,7 @@ library(dplyr)
 # for the GR dataset, trapseason 2 correponds to season 2 - season 1, thus it needs to be removed
 
 
-filtdataab <- select(ABDF_h, -`16daysperiod.sum`)
+filtdataab <- ABDF_hm
 filtdatagr <- select(GRDF_h, -`16daysperiod.sum`)
 
 names(filtdataab)
@@ -19,7 +19,7 @@ modcoefs <- matrix(NA,ncol=3,nrow=8)
 
 i = 1
 par(mfrow=c(2,4))
-plotlab <- c("Setting Day","Experiment Days","Previous 3 Days","Previous 5 Days", "Previous 10 Days",
+plotlab <- c("Previous Day","Experiment Days","Previous 3 Days","Previous 5 Days", "Previous 10 Days",
              "+-1 Days Interval","+-3 Days Interval","+-8 Days Interval")
 for(i in 1:nrow(modcoefs))
 {
@@ -32,32 +32,32 @@ for(i in 1:nrow(modcoefs))
   colnames(newAB)[4] = colnames(newGR)[4] = "var"
   
   modGR <- npreg::ss(newGR$Dens,newGR$var
-                     ,spar=.6
+                     ,method="REML"
                      )
   modAB <- npreg::ss(newAB$Dens,newAB$var
-                     , spar=.6
+                     ,method="REML"
                      )
   
   modcoefs[i,1] <- names(filtdataab)[38+i]
   modcoefs[i,2] <- summary(modAB)$r.squared
   modcoefs[i,3] <- summary(modGR)$r.squared
-  
+  # 
   # plot(modGR, main=plotlab[i], ylab="Density Index (GR)",
-  #      xlab="Camera Triggers (GR)")
+  #      xlab="Camera Triggers (GR)", ylim=c(-2,2))
   # points(newGR$Dens,newGR$var, pch=19, col=scales::alpha(3,.4))
   # 
   # abline(0,1,lty=2)
-  # r2x <- round(summary(modGR)$r.squared,3)
-  # mtext(bquote(R^2==.(r2x)), cex=.8)
-  
+  # r2x <- round(summary(modGR)$adj.r.squared,3)
+  # mtext(bquote(adj~R^2==.(r2x)), cex=.8)
+  # 
   #PLOT Density
-  plot(modAB, main=plotlab[i], xlab="Density Index",
-  ylab="Camera Triggers")
+  plot(modAB, main=plotlab[i], xlab="CR Density",
+  ylab="CT Density Index", ylim=c(0,15))
   points(newAB$Dens,newAB$var, pch=19, col=scales::alpha(3,.4))
   abline(0,1,lty=2)
-  r2x <- round(summary(modAB)$r.squared,3)
-  mtext(bquote(R^2==.(r2x)), cex=.8)
-  # 
+  r2x <- round(summary(modAB)$adj.r.squared,3)
+  mtext(bquote(adj~R^2==.(r2x)), cex=.8)
+  #
   
 }
 
