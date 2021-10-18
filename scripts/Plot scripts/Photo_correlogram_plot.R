a2d93edb283f07f8444a0f03a99ced4987ab0942
pedrognicolau <pedrognicolau@gmail.com>
2021-10-18 13:34:51 +0200

    correlogram script

--- scripts/Plot scripts/Photo_correlogram_plot.R
+++ scripts/Plot scripts/Photo_correlogram_plot.R
@@ -1,42 +1,11 @@
 # Correlogram plot ####
+GSVdata <- readRDS("data/cameratrap/porsanger/processed/GS_photos_porsanger.rds")
+TVdata <- readRDS("data/cameratrap/haakoya/processed/tv_photos_haakoya.rds")
 
-cameradata <- readRDS("data/cameratrap/porsanger/processed/porsanger_camera_processing1.rds")
-cameradata$DateTimeOriginal <- as.POSIXct(cameradata$DateTimeOriginal)
-DT <- data.table(cameradata)
-cameradata$date <- as.Date(format(cameradata$DateTimeOriginal,format='%Y/%m/%d'))
-cameradata$count <- 1
-unique(cameradata$species)
-cameracounts <- aggregate(count ~ date+station+species, data= cameradata, FUN=sum)
-GSVcounts <- arrange(filter(cameracounts, species=="G"),date,station)
-
-
-GSVdata <- readRDS("data/GS_photos_porsanger.rds")
+# cameras were not in place yet
 GSVdata[GSVdata$trapseason==1,][,4:13] <- NA
 
-
-
-cor(c(0,0,7,0),c(0,1,0,0))
-
-sync_df5 <- function(res, round=0, dist_mat=gps$cumdist, method="pearson")
-  # residuals as a smooth function of distance
-{
-  distmat <- stats :: dist(dist_mat, upper = TRUE, diag = TRUE)
-  m <- data.frame(t(combn(as.numeric(rownames(gps)),2)), dist=as.numeric(distmat))
-  
-  CR2 <- cor(res, method=method)
-  CR2[upper.tri(CR2)] <- NA
-  diag(CR2) <- NA
-  nCR <- reshape2::melt(CR2, varnames = c('X2', 'X1'), na.rm = TRUE)
-  colnames(nCR)[3] <- "corres"
-  
-  mat2 <- dplyr::left_join(m,nCR)
-  mat3 <- arrange(mat2, dist)
-  mat3$dist <- round(mat3$dist, round)
-  mat4 <- aggregate(corres~dist, data=mat3, FUN=mean)
-  
-  return(mat4)
-}
-
+# create distance matrix (time distance matrix)
 DMAT <-outer(-10:10, -10:10, '-')
 DMAT2 <- DMAT[lower.tri(DMAT, diag = FALSE)]
 rownames(DMAT) <- paste0("d",-10:10)
@@ -45,14 +14,50 @@ diag(DMAT)
 DMAT[upper.tri(DMAT, diag = TRUE)]
 m <- data.frame(t(combn(rownames(DMAT),2)), dist=DMAT2)
 
-cor(GSVdata[,4:24], use = "pairwise.complete.obs")
 
-CR2 <- cor(GSVdata[,4:24], use = "pairwise.complete.obs")
+# voledata <- GSVdata[,4:24]
+voledata <- TVdata[,3:23]
+
+CR2 <- cor(voledata, use = "pairwise.complete.obs")
 CR2[upper.tri(CR2)] <- NA
 diag(CR2) <- NA
 nCR <- reshape2::melt(CR2, varnames = c('X2', 'X1'), na.rm = TRUE)
-colnames(nCR)[3] <- "corres"
 mat2 <- dplyr::left_join(m,nCR)
 
 plot(mat2$dist,mat2$value)
 lines(smooth.spline(mat2$dist,mat2$value), col=2)
+
+
+i=1
+par(mfrow=c(2,3))
+stationsRJ <- c("Rolf","Rolf","Rolf","Jon")
+for(i in 1:4)
+{
+  plotdata <- filter(TVdata, station==i)
+
+  voledata <- plotdata[,3:23]
+  
+  CR2 <- cor(voledata, use = "pairwise.complete.obs")
+  CR2[upper.tri(CR2)] <- NA
+  diag(CR2) <- NA
+  nCR <- reshape2::melt(CR2, varnames = c('X2', 'X1'), na.rm = TRUE)
+  mat2 <- dplyr::left_join(m,nCR)
+  
+  plot(mat2$dist,mat2$value, ylim=c(-.2,1), xlab = "Days apart", ylab="Correlation in Number of Photos", 
+       main=paste0(stationsRJ[i]," (Station ", i,")"))
+  lines(smooth.spline(mat2$dist,mat2$value), col=2)
+  
+  abline(h=0.5,lty=2)
+}
+
+voledata <- GSVdata[,4:24]
+CR2 <- cor(voledata, use = "pairwise.complete.obs")
+CR2[upper.tri(CR2)] <- NA
+diag(CR2) <- NA
+nCR <- reshape2::melt(CR2, varnames = c('X2', 'X1'), na.rm = TRUE)
+mat2 <- dplyr::left_join(m,nCR)
+
+plot(mat2$dist,mat2$value, ylim=c(-0.2,1), xlab = "Days apart", ylab="Correlation in Number of Photos", 
+     main="Porsanger All Stations")
+lines(smooth.spline(mat2$dist,mat2$value), col=2)
+
