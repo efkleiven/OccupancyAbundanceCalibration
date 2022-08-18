5cf475cfe4ee69d8a14c4d120a962b9b8705676f
Eivind Flittie Kleiven <ekl013@uit.no>
2022-08-18 15:30:20 +0200

    update plotting script

--- scripts/Plot scripts/plot_compare_CTcounts_CMRabundance_3.R
+++ scripts/Plot scripts/plot_compare_CTcounts_CMRabundance_3.R
@@ -110,43 +110,36 @@ legend("topright", legend=c("CMR-abundance","CT-counts"), col=c("orange", "skybl
 ## With SE bars
 
 setwd("C:/Users/ekl013/OneDrive - UiT Office 365/GitProjects/OccupancyAbundanceCalibration/plots")
-png("TimeSeries_LogCTcountsCRabundance_2.png", width = 1200, height=600)
+png("TimeSeries_LogCTcountsCRabundance_3.png", width = 1200, height=600)
 par(mfrow=c(1,2), mar=c(5,7,4,5))
 
 library(gplots)
 
-#gplots :: plotCI(as.Date(timelab$date),log(vole2$count/15+1),vecsd_po, col="skyblue", ylim=c(-0.9,5), ylab="CT-index / CMR abundance", xlab="",
-#                 pch=19, cex.main=1.5, cex.lab=1.5,cex.axis=1.5 , cex=2, main="Gray-sided vole", xaxt='n', yaxt='n', type="b", lty=2) 
+plotCI(as.Date(time), pormean2_ct$int_2, po_se_ct$int_2, col="skyblue", ylim=c(0,3), ylab="", xlab="", pch=19, cex.main=1.5, cex.lab=1.5,cex.axis=1.5 , cex=2, main="Gray-sided vole", 
+                 xaxt='n', yaxt='n', type="p",  lwd=2.5, gap=0)
+lines(pormean2_ct$int_2~as.Date(time), lty=2, col="skyblue", lwd=2)
 
-plotCI(as.Date(time), pormean2_ct$int_2, po_se_ct$int_2, col="skyblue", ylim=c(0,3), ylab="CT-index / CMR abundance", xlab="", pch=19, cex.main=1.5, cex.lab=1.5,cex.axis=1.5 , cex=2, main="Gray-sided vole", 
-                 xaxt='n', yaxt='n', type="b",  lwd=2.5, gap=0) 
-plotCI(as.Date(time)+10, pormean2_cmr$Abundance_HT, po_se_cmr$Abundance_HT, col="orange", ylim=c(0,4), ylab="CT-index", cex.lab=1.3, pch=19, cex=2, add=T, type="b", lwd=2.5, gap=0) 
+plotCI(as.Date(time)+10, pormean2_cmr$Abundance_HT, po_se_cmr$Abundance_HT, col="orange", ylim=c(0,4), cex.lab=1.3, pch=19, cex=2, add=T, type="p", lwd=2.5, gap=0) 
+
+time2 <- as.Date(time)+10
+lines(pormean2_cmr$Abundance_HT ~ (as.Date(time2)), lty=2, col="orange", lwd=2)
+
 axis(1, at=as.Date(xlab), lab=c("2018", "2019", "2020"),  cex.axis=1.5)
 axis(2, at=c(0,1,2,3,4), lab=c(0,1,2,3,4),  cex.axis=1.5)
 
-
-#plot(log(vole2$count/15+1)~as.Date(timelab$date), type="b",pch=16, lwd=2, col="skyblue", ylim=c(0,4),lty=2 ,xlab="", ylab="CT-index / CMR-abundance",cex.main=1.5, cex.lab=1.5,cex.axis=1.5 , cex=2, main="Gray-sided vole", xaxt='n')
-#lines(log(N.est/15+1)~time, data=CMR, type="b", lty=2, pch=17,cex=2, lwd=2, col="orange")
-#axis(1, at=as.Date(xlab), lab=c("2018", "2019", "2020"),  cex.axis=1.5)
-#legend("topleft", legend=c("CMR-abundance","CT-index"), col=c("orange", "skyblue"), pch=c(17,16), cex=1.3)
-
-#mtext("CMR-Abundance",side=4, padj=3, cex=1.5 )
-#mtext("CT-counts",side=2, padj=-3.5 , cex=1.5)
 
 # Tundra vole
-plotCI(time_h,hamean_ct$previousday,se_ct$previousday, col="skyblue", ylim=c(0,3), ylab="CT-index / CMR abundance", xlab="", pch=19, cex.main=1.5, cex.lab=1.5,cex.axis=1.5 , cex=2, main="Tundra vole", xaxt='n', yaxt='n', type="b", lwd=2.5, gap=0) 
-plotCI(time_h+10,hamean_cmr$D50, se_cmr$D50, col="orange", ylim=c(0,3), ylab="CT-index", cex.lab=1.3, pch=19, cex=2, add=T, type="b", lwd=2.5, gap=0) 
+plotCI(time_h,hamean_ct$previousday,se_ct$previousday, col="skyblue", ylim=c(0,3), ylab="", xlab="", pch=19, 
+       cex.main=1.5, cex.lab=1.5,cex.axis=1.5 , cex=2, main="Tundra vole", xaxt='n', yaxt='n', type="p", lwd=2.5, gap=0)
+lines(hamean_ct$previousday~time_h, lty=2, col="skyblue", lwd=2)
+
+plotCI(time_h+10,hamean_cmr$D50, se_cmr$D50, col="orange", ylim=c(0,3), ylab="CT-index", cex.lab=1.3, pch=19, cex=2, add=T, type="p", lwd=2.5, gap=0) 
+lines(hamean_cmr$D50~time_h, lty=2, col="orange", lwd=2)
+
 axis(1, at=as.Date(xlab), lab=c("2018", "2019", "2020"),  cex.axis=1.5)
 axis(2, at=c(0,1,2,3,4), lab=c(0,1,2,3,4),  cex.axis=1.5)
-legend("topright", legend=c("CMR-abundance","CT-index"), col=c("orange", "skyblue"), pch=c(17,16), cex=1.3)
+legend("topright", legend=c("log(CMR-abundance)","log(CT-index)"), col=c("orange", "skyblue"), pch=c(17,16), cex=1.3)
 
-#par(mar=c(5,4,4,3.5))
-#plot(log(CT_all_h$count/4+1) ~ datesR$date, type="b", pch=16,lwd=2, col="skyblue", ylim=c(0,4),xlab="",lty=2, ylab="",cex.main=1.5, cex.lab=1.5, cex.axis=1.5, cex=2, main="Tundra vole", xaxt='n')
-#lines(log(CMR_h$inlacr/4+1) ~ datesR$date,type="b", lty=2, lwd=2, pch=17,cex=2, col="orange")
-#axis(1, at=as.Date(xlab), lab=c("2018", "2019", "2020"),  cex.axis=1.5)
-#axis(4, at=c(20,40,60,80,100), lab=c(20,40,60,80,100)*0.5,cex.axis=1.5)
-#mtext("CMR-Abundance",side=4, padj=3.5, cex=1.5)
-#mtext("CT-counts",side=2, padj=-3.5, cex=1.5)
 
 dev.off()
 
